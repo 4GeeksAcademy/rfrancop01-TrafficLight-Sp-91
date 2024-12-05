75c2388058cad447959212c7af282a95cdc4d055
Ricardo Franco Pérez <rfrancop01@gmail.com>
2024-12-05 20:31:01 +0000

    Termino el semáforo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	const [light, setLight] = useState()
+
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div className="row container bg-dark mx-auto mt-4 p-2 d-flex justify-content-center" style={{ width: "10px", height: "100px" }} ></div>
+			<div class="row container bg-dark mx-auto p-2 d-flex justify-content-center rounded" style={{ width: "100px", height: "200px" }}>
+				<div className="light bg-danger rounded-circle mt-2 shadow bg-body-tertiary" style={{ width: "50px", height: "50px" }}></div>
+				<div className="light bg-warning rounded-circle" style={{ width: "50px", height: "50px" }}></div>
+				<div className="light bg-success rounded-circle" style={{ width: "50px", height: "50px" }}></div>
+			</div>
 		</div>
 	);
 };
